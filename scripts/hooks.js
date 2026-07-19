db8c66efa3ddb61c3eb2c4ef659a7c1131cf4cf9
stop666two <stop666bilibili@gmail.com>
2026-07-19 13:19:10 +0800

    ai: chore: remove stale hooks.js stub - build.js gracefully handles missing hooks via try/catch

--- scripts/hooks.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preBuild: async (config) => {},
-  postBuild: async (config, stats) => {},
-  transformMarkdown: (content, frontmatter) => content,
-  transformHTML: (html, pageContext) => html
-};
